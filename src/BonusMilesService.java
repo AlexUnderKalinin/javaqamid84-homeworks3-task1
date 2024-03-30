97a47bd453a1e161018b7c1489878bf5d92f522b
Alexander Kalinin <legalcounsel42@gmail.com>
2024-03-30 23:42:47 +0300

    Update bonus miles application

---
+++ src/BonusMilesService.java
@@ -0,0 +1,9 @@
+public class BonusMilesService {
+
+    int a = 20;
+        public int calculate(int price) {
+            int result;
+            result = price / a;
+        return result;
+    }
+}
